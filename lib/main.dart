1db5d659a05282f2909385a287a67b9de73d070f
MDar123 <mujtabadar98@gmail.com>
2024-10-27 21:16:27 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:first_project/currency_converter_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const mob());
+  runApp(const Myapp());
 }
 
 // ignore: camel_case_types
-class mob extends StatelessWidget {
-  const mob({super.key});
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
